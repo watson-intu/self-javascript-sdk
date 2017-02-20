422b1dbcf181dc1bb7202bff4a96e941c2117d70
Ravi Sinha <rssinha@us.ibm.com>
2017-02-20 11:39:43 -0600

    Add WebSocket connection to local Intu

--- src/js/TopicClient.js
+++ src/js/TopicClient.js
@@ -4,6 +4,24 @@ selfId='';
 token='';
 orgId='';
 
+function sendMessage(webSocket, message) {
+	// Construct a message object containing the data the server needs
+	var msg = {
+		type: 'Message',
+		text: message,
+	}
+
+	webSocket.send(JSON.stringify(msg));
+
+}
+
 function sayHelloToIntu(message) {
-	alert(message);
+	var intuSocket = new WebSocket('ws://' + host + ':' + port + '/stream?selfId=&orgId=&token=')
+	sendMessage(intuSocket, message);
+	console.log(intuSocket.toString());
+
+	intuSocket.onmessage = function(event) {
+		console.log('Received a response from the server!');
+		console.log(JSON.parse(event.data));
+	}
 }
