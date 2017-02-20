413f4a536d6f52d8afbf4164e61677125c89c78c
Ravi Sinha <rssinha@us.ibm.com>
2017-02-20 13:32:08 -0600

    Add handshake and subscription to topics

--- src/js/TopicClient.js
+++ src/js/TopicClient.js
@@ -4,24 +4,53 @@ selfId='';
 token='';
 orgId='';
 
-function sendMessage(webSocket, message) {
+function doHandshake(webSocket, message) {
 	// Construct a message object containing the data the server needs
 	var msg = {
-		type: 'Message',
-		text: message,
-	}
+		"targets": [""],
+		"msg": "query",
+		"request": "1",
+		"origin": "/."
+	};
 
+	console.log('Sending message: ' + JSON.stringify(msg));
 	webSocket.send(JSON.stringify(msg));
+}
 
+function requestSubscription(webSocket, subscriptionType) {
+	// Construct a query object containing the type of connection that is desired
+
+	var msg = {
+		"targets": ["\"" + subscriptionType + "\""],
+		"msg": "subscribe",
+		"origin": "/."
+	}
 }
 
 function sayHelloToIntu(message) {
-	var intuSocket = new WebSocket('ws://' + host + ':' + port + '/stream?selfId=&orgId=&token=')
-	sendMessage(intuSocket, message);
-	console.log(intuSocket.toString());
+	var intuSocket = new WebSocket('ws://' + host + ':' + port + '/stream?selfId='
+				       + selfId + '&orgId=' + orgId + '&token=' + token)
+
+	intuSocket.onopen = function(event) {
+		doHandshake(intuSocket, message);
+		console.log('Performing handshake:' + intuSocket.toString());
+		console.log('Subscribing to the log')
+		requestSubscription(intuSocket, 'log')
+		console.log('Subscribing to the blackboard stream')
+		requestSubscription(intuSocket, 'blackboard-stream')
+	}
 
 	intuSocket.onmessage = function(event) {
 		console.log('Received a response from the server!');
 		console.log(JSON.parse(event.data));
+	}
+
+	intuSocket.onerror = function(event) {
+		console.log('An error occurred: ' + JSON.parse(event.data));
+	}
+
+	intuSocket.onclose = function(event) {
+		console.log('Closing the connection: ' + JSON.parse(event.data));
+		intuSocket.close();
 	}
 }
