2302f8e03d605bdb6a9f5b9aaeed56314ce8d264
John Andersen <jjanders@us.ibm.com>
2017-02-23 11:17:55 -0600

    INTU-578 | Working base SensorManager

--- src/js/SensorManager.js
+++ src/js/SensorManager.js
@@ -19,22 +19,60 @@ SensorManagerInstance.prototype = {
 		sensorOverrideMap.put(sensor.sensorId, override);
 	},
 
+	isRegistered: function(sensor) {
+		var found = sensorMap.get(sensor.sensorId);
+		if(found == undefined) {
+			return false;
+		}
+		return true
+	},
+
+	sendData: function(sensor, data) {
+		if(isRegistered(sensor)) {
+			TopicClient.getInstance().publish("sensor-proxy-" + sensor.sensorId, data, false);
+		}
+	},
+
+	removeSensor: function(sensor) {
+		if(sensorMap.get(sensor.sensorId) != undefined) {
+			sensorMap.remove(sensor.sensorId);
+			sensorOverrideMap.remove(sensor.sensorId);
+			var msg = {
+				"event" : "remove_sensor_proxy",
+				"sensorId" : sensor.sensorId
+			};
+			TopicClient.getInstance().publish("sensor-manager", msg, false);
+		}
+	},
+
+	findSensor: function(dataType) {
+		for(var i = 0; i++ < sensorMap.size; sensorMap.next()) {
+			var sensor = sensorMap.value();
+			if(sensor.dataType == dataType) {
+				return sensor;
+			}
+		}
+
+		return undefined;
+	},
+
 	getSensors: function() {
 		for(var i = 0; i++ < sensorMap.size; sensorMap.next()) {
 			console.log(sensorMap.key() + ": " + sensorMap.value());
 		}
 	},
 
-	getTest: function() {
-		console.log("getTest hit!");
+	onEvent: function(msg) {
+		console.log("onEvent hit in SensorManager!");
+		console.log(msg);
 	},
 
-	getTest2: function() {
-		console.log("getTest2 hit!");
+	shutdown: function() {
+		TopicClient.getInstance().unsubscribe("sensor-manager");
 	},
 
 	start: function() {
-		TopicClient.getInstance().subscribe("sensor-manager", getTest);
+		TopicClient.getInstance().subscribe("sensor-manager", onEvent);
 	}
 }
 
