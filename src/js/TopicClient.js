4882471bf81a751d4cc75f0804a9b1545977174e
Ravi Sinha <rssinha@us.ibm.com>
2017-02-23 11:30:07 -0600

    Update TopicClient to follow Singleton

--- src/js/TopicClient.js
+++ src/js/TopicClient.js
@@ -5,6 +5,32 @@ token='';
 orgId='';
 
 function TopicClientInstance() {
+	this.socket = new WebSocket('ws://' + host + ':' + port + '/stream?selfId=' + selfId + '&orgId=' + orgId + '&token=' + token);
+	this.socket.onopen = function(event) {
+		console.log('Performing handshake:');
+		doHandshake(this.socket, message);
+		console.log('Handshake complete, readyState = ' + this.socket.readyState);
+		console.log('Protocol selected by the server = ' + this.socket.protocol);
+		console.log('Subscribing to the log');
+		requestSubscription(this.socket, 'log');
+		console.log('Subscribing to the blackboard stream');
+		requestSubscription(this.socket, 'blackboard-stream');
+	}
+
+	this.socket.onmessage = function(event) {
+		console.log('Received a response from the server!');
+		console.log(JSON.parse(event.data));
+	}
+
+	this.socket.onerror = function(event) {
+		console.log('An error occurred: ' + JSON.parse(event.data));
+	}
+
+	this.socket.onclose = function(event) {
+		console.log('Closing the connection: ' + JSON.parse(event.data));
+		socket.close();
+	}
+
 	console.log("TopicClient has been instantiated!!");
 }
 
@@ -12,11 +38,32 @@ TopicClientInstance.prototype = {
 	constructor: TopicClientInstance,
 
 	subscribe: function(path, callback) {
+		var msg = {
+			"targets": [subscriptionType],
+			"msg": "subscribe",
+			"origin": "/."
+		};
+		this.socket.send(JSON.stringify(msg));
 		console.log("TopicClient subscription called!");
 	},
 
 	publish: function(path, msg, persisted) {
 		console.log("TopicClient publishing data!");
+	},
+
+	onPong: function(buffer) {
+		console.log("TopicClient onPong called");
+	},
+
+	doHandshake: function() {
+		var msg = {
+			"targets": [""],
+			"msg": "query",
+			"request": "1",
+			"origin": "/."
+		};
+
+		this.socket.send(JSON.stringify(msg));
 	}
 }
 
@@ -39,58 +86,3 @@ var TopicClient = (function () {
 	};
 })();
 
-
-
-function doHandshake(webSocket, message) {
-	// Construct a message object containing the data the server needs
-	var msg = {
-		"targets": [""],
-		"msg": "query",
-		"request": "1",
-		"origin": "/."
-	};
-
-	console.log('Sending message: ' + JSON.stringify(msg));
-	webSocket.send(JSON.stringify(msg));
-}
-
-function requestSubscription(webSocket, subscriptionType) {
-	// Construct a query object containing the type of connection that is desired
-
-	var msg = {
-		"targets": [subscriptionType],
-		"msg": "subscribe",
-		"origin": "/."
-	};
-	webSocket.send(JSON.stringify(msg));
-}
-
-function sayHelloToIntu(message) {
-	var intuSocket = new WebSocket('ws://' + host + ':' + port + '/stream?selfId='
-				       + selfId + '&orgId=' + orgId + '&token=' + token);
-
-	intuSocket.onopen = function(event) {
-		console.log('Performing handshake:' + intuSocket.toString());
-		doHandshake(intuSocket, message);
-		console.log('Handshake complete, readyState = ' + intuSocket.readyState);
-		console.log('Protocol selected by the server = ' + intuSocket.protocol);
-		console.log('Subscribing to the log');
-		requestSubscription(intuSocket, 'log');
-		console.log('Subscribing to the blackboard stream');
-		requestSubscription(intuSocket, 'blackboard-stream');
-	}
-
-	intuSocket.onmessage = function(event) {
-		console.log('Received a response from the server!');
-		console.log(JSON.parse(event.data));
-	}
-
-	intuSocket.onerror = function(event) {
-		console.log('An error occurred: ' + JSON.parse(event.data));
-	}
-
-	intuSocket.onclose = function(event) {
-		console.log('Closing the connection: ' + JSON.parse(event.data));
-		intuSocket.close();
-	}
-}
